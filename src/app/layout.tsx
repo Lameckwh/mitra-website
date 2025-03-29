985ed64b5398fcbf7202a7ee95e8af1b3900c9c3
Lameck WH Mbewe <68487945+Lameckwh@users.noreply.github.com>
2025-03-29 22:20:27 +0200

    feat: integrate NProgress for loading indicators and enhance layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,15 @@ import { ReactNode } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { ThemeProvider } from "next-themes";
+import { Metadata } from "next";
 
 interface RootLayoutProps {
   children: ReactNode;
 }
-
+export const metadata: Metadata = {
+  title: 'Mitra Systems Ltd',
+  description: 'Enterprise-level server, virtualization, storage, and security solutions.',
+};
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
